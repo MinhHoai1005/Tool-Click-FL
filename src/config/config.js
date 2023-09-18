f011c7bb5ff894a40b92e6c99d7e0153351e1932
hoaivm <hoaivm@ghn.vn>
2023-09-19 03:51:44 +0700

    login

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,15 @@
 export const domainWeb = 'http://localhost:8085/public-api/';
 
+export const Firebase = {
+  apiKey: "AIzaSyC2LmCe5Tv9yFnSDN1730FCCb12mHEGZcw",
+  authDomain: "toolplus-3ea83.firebaseapp.com",
+  databaseURL: "https://toolplus-3ea83-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "toolplus-3ea83",
+  storageBucket: "toolplus-3ea83.appspot.com",
+  messagingSenderId: "882982821045",
+  appId: "1:882982821045:web:8af58412fae4b69519f2fc",
+  measurementId: "G-19RC86872K"
+}
 export const api = {
   createAccount: {
     url: domainWeb + 'account/create',
--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -3,8 +3,16 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import "./styles.css"
 import { useNavigate } from "react-router-dom";
 import { login } from 'store/Auth/sagas';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
+import { Firebase } from 'config/config';
 
-function LoginPage () {
+const app = initializeApp(Firebase);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const providerfb = new FacebookAuthProvider();
+
+function LoginPage() {
   const navigate = useNavigate();
 
   const [inputs, setInputs] = useState({
@@ -22,7 +30,28 @@ function LoginPage () {
       // checkLogin()
     }
   };
+  const signInWithFacebook = async () => {
+    console.log("FB")
+    try {
+      await signInWithPopup(auth, provider);
+      console.log('Đăng nhập thành công!');
+    } catch (error) {
+      console.error('Lỗi đăng nhập:', error);
+    }
+  };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, providerfb);
+      const user = result.user;
+      console.log('Đăng nhập thành công:', user);
+      navigate('/admin');
+    } catch (error) {
+      console.error('Lỗi đăng nhập:', error);
+    }
+  };
+
+  
   useEffect(() => {
     const token = localStorage.getItem('token');
 
@@ -63,8 +92,8 @@ function LoginPage () {
           Login In
         </button>
         <div className="social">
-          <div className="go"><i className="fab fa-google" /> Google</div>
-          <div className="fb"><i className="fab fa-facebook" /> Facebook</div>
+          <div className="go" onClick={handleGoogleLogin} ><i className="fab fa-google" /> Google</div>
+          <div className="fb"  onClick={signInWithFacebook} ><i className="fab fa-facebook"/> Facebook</div>
         </div>
       </form>
     </div >
